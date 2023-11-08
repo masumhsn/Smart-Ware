13bf3ee50d36ac27386fb655b7f1859eadb82a1d
Masum <masumhsn@gmail.com>
2023-11-08 02:18:19 -0500

    test commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [ipCount, setIpCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <button> +1 IP </button>
+        <button
+          style={{
+            backgroundColor: '#61DBFB',
+            padding: '10px 20px',
+            color: 'white',
+            border: 'none',
+            borderRadius: '20px',
+            fontWeight: 'bold'
+          }}
+          onClick={()=> setIpCount(ipCount+1)}
+        > 
+          +1 IP 
+        </button>
+        <p>IP Count: {ipCount}</p>
       </header>
     </div>
   );
